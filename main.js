73003253c0794cda1190a2ad105c8e86b0608680
Harmangulati <83772120+Harmangulati@users.noreply.github.com>
2022-01-04 19:17:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.getElementById("result").innerHTML='<img id="snapshot" src="'+data_uri+
 });    
 }
 console.log('ml5 version', ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tJIG5lcL0/.json', modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tJIG5lcL0/model.json', modelLoaded);
 function modelLoaded(){
     console.log("ModelLoaded");
 }
@@ -34,8 +34,7 @@ synth.speak(utterThis);
      }
      else {
          console.log(results);
-    document.getElementById("result_emotion_name").innerHTML
+    document.getElementById("result_emotion_name").innerHTML=
     
 prediction_1=results[0].label;
-prediction_2=results[1].label;
 speak();
